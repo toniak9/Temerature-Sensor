1b3a1d2846817f8c57de7c11eb34c2d023bac39e
toniak9 <toniak19@gmail.com>
2016-03-02 00:08:45 -0800

    comments added

--- app/src/main/java/com/example/tonia/Temperature/Temperature.java
+++ app/src/main/java/com/example/tonia/Temperature/Temperature.java
@@ -16,11 +16,18 @@ import android.widget.TextView;
 
 import java.util.Random;
 
+/**
+ * This app converts the temperature froom °C to °F and vice-versa.
+ * This app displays the temperatures of fvr days in a week (Monday to Friday)
+ * It also displays the ambient temperature for SDK versions from 19
+ */
 
 public class Temperature extends Activity implements SensorEventListener {
+
     public boolean isCelcius = true;
     TextView temperature = null;
     TextView ambientTemp = null;
+
     private SensorManager sensor;
     TableLayout table = null;
     TableRow row = null;
@@ -30,32 +37,55 @@ public class Temperature extends Activity implements SensorEventListener {
 
         super.onCreate(savedInstanceState);
         setTitle("Temperature Convertor");
+
+        //setting the layout for the activity
         setContentView(R.layout.activity_temperature);
         table = (TableLayout) findViewById(R.id.tableLayout);
+
+        /**
+         * rand() function is used to generate random temperatures for five days in a week
+         * Temperatures are displayed from Monday to Friday in °C when the activity starts
+         */
         Random rand = new Random();
 
+        // Sensors are used to display the ambient temperature
         Sensor ambientTemperature;
         sensor = (SensorManager) getSystemService(SENSOR_SERVICE);
         ambientTemperature = sensor.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
         ambientTemp = (TextView) findViewById(R.id.ambientTemp);
 
+        //check if ambient temperature feature is supported by the phone
         if(ambientTemperature == null)
             ambientTemp.setText("Ambient Temperature Not supported");
 
+        /**
+         * Here, a Table Layout is used to display the days along with corresponding temperatures
+         * first row, row0 of the table is a header
+         * So, the table is iterated to get each row and generate a random temperature
+         * for that day and populate the TextView value with it
+         */
         for (int i = 1; i < table.getChildCount(); i++) {
             row = (TableRow) table.getChildAt(i);
+            //child 1 is the 2nd column, the temperature field
             temperature = (TextView) row.getChildAt(1);
             double temp = rand.nextDouble() * 100;
             temperature.setText(Double.toString(temp));
         }
     }
+
+    /**
+     *
+     * @param v
+     * onButtonClick, some even should happen.
+     * Here, the teperature is converted from °C to °F or vice-vera
+     */
         public void onClick(View v){
             TextView temp;
             TextView extension;
-            TextView headerTemp;
             double changedTemp = 0.0;
 
             Button button = (Button) v;
+            //get the corresponding row of button click
             TableRow tableRow = (TableRow) v.getParent();
             temp = (TextView) tableRow.getChildAt(1);
             extension = (TextView) tableRow.getChildAt(2);
@@ -66,6 +96,7 @@ public class Temperature extends Activity implements SensorEventListener {
                 button.setText("To °C");
                 double temperature = Double.parseDouble(temp.getText().toString().trim());
                 changedTemp = (temperature * 1.8) + 32;
+                //update the temperature value (from °C to °F)
                 temp.setText(Double.toString(changedTemp));
                 extension.setText("°F");
 
@@ -77,6 +108,7 @@ public class Temperature extends Activity implements SensorEventListener {
                 button.setText("To °F");
                 double temperature = Double.parseDouble(temp.getText().toString().trim());
                 changedTemp = (temperature - 32) / (1.8);
+                //update the temperature value (from °F to °C)
                 temp.setText(Double.toString(changedTemp));
                 extension.setText("°C");
             }
